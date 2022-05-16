f69773b2ed1a1894c3d90fbec3f6eb5c9fbf49d1
Masaya Ueda <88300005+yanmasa@users.noreply.github.com>
2022-05-16 13:21:29 +0900

    StyledComponents

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { CssModules } from "./components/CssModules";
 import { InlineStyle } from "./components/inlineStyle";
+import { StyledComponents } from "./components/StyledComponents";
+import { StyledJsx } from "./components/StyledJsx";
 import "./styles.css";
 
 export default function App() {
@@ -7,6 +9,8 @@ export default function App() {
     <div className="App">
       <InlineStyle />
       <CssModules />
+      <StyledJsx />
+      <StyledComponents />
     </div>
   );
 }
--- src/components/CssModules.jsx
+++ src/components/CssModules.jsx
@@ -3,7 +3,7 @@ import classes from "./CssModules.module.scss";
 export const CssModules = () => {
   return (
     <div className={classes.container}>
-      <p className={classes.title}>- Inline Styles -</p>
+      <p className={classes.title}>- CSS Modules -</p>
       <button className={classes.button}>FIGHT</button>
     </div>
   );
